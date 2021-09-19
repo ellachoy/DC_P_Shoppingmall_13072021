b20f92951abb286ad8dbfcb456546bddc35371fd
Ella Choy <egal21de@gmail.com>
2021-09-19 18:07:10 +0200

    js_up

--- src/main.js
+++ src/main.js
@@ -6,14 +6,17 @@ function loadItems() {
    .then(json => json.items); //return only intems in json
 }
 
-
+//Update the list with given items
 function displayItems(items){ //3-1konvert html element -> in brower anzeigen lassen
    const container =document.querySelector('.items');//1. needed container to add items in parents container
    // console.log(container);
    //container 에 innerHTML을 이용해서, 받아온 아이템들을 li그룹으로 만들어서, container 에추가
    //아이템즈 배열 을  HTML li tag로 변환해주는 배열을 만듬.
     //한가지의 배열에서 다른 형태로 변환 맵핑 
-   container.innerHTML = items.map(item =>createHTMLString(item)).join('');// li tag로 변환
+    // join으로 배열 스트링으로 
+    //don´t understand? chcek mit const html 
+    const html =items.map(item =>createHTMLString(item));
+   container.innerHTML = items.map(item =>createHTMLString(item)).join('');// li tag로 변환  
    
 }
 // li tag로 변환.위해 문자열 스트링을 만듬 중요= 뻭틱기호.
